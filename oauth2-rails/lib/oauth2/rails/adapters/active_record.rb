8ff1c5f773aecae5bac7c16a10228af7005f1171
Alexander Flatter <flatter@gmail.com>
2010-07-06 20:11:48 +0200

    Remove useless stuff

--- oauth2-rails/lib/oauth2/rails/adapters/active_record.rb
+++ oauth2-rails/lib/oauth2/rails/adapters/active_record.rb
@@ -13,27 +13,3 @@ module OAuth2
   end
 
 end
-
-@adapter = Class.new(OAuth2::Rails::Adapter) do
-  %w{client user}.each do |model|
-    adapter = OAuth2::Rails.models[model][:adapter].to_s.camelize
-    klass   = OAuth2::Rails.models[model][:klass].to_s.camelize.constantize
-    include "OAuth2::Rails::Adapters::#{adapter}::#{model}".camelize.constantize
-    
-    self.send "#{model}_klass=".to_sym, klass
-  end
-end
-
-OAuth2::Rails.setup do |config|
-  
-  # Override the default adapter. By default, it is set to your current ORM.
-  config.adapter = :active_record
-  
-  # Override 
-  config.models[:client] = :client
-  
-  # Namespaced model and adapter names
-  config.models[:client] = :'my/client'
-  config.adapter = :'my/active_record'
-  
-end
