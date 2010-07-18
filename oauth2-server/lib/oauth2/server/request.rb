f608243c70515b581e952dfac808c31acab3434c
DBA <diogo.borges.almeida@gmail.com>
2010-07-18 10:49:38 +0100

    Request - Added class description

--- oauth2-server/lib/oauth2/server/request.rb
+++ oauth2-server/lib/oauth2/server/request.rb
@@ -1,6 +1,9 @@
 module OAuth2
   module Server
 
+    # +Request+ is the gem level representation of the HTTP Request received by your application.
+    #
+    # OAuth2-Rails translates ActionDispatch::Request into OAuth2::Server::Request
     class Request
     end
 
